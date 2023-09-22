809214659f84d12b8beb5c7d7468202cb3a47322
Dan Yamamoto <dan@iij.ad.jp>
2023-09-22 06:45:53 +0000

    reorganize tests

--- tests/js/proofs.spec.ts
+++ tests/js/proofs.spec.ts
@@ -1,14 +1,8 @@
 import {
   DeriveProofRequest,
-  blindSignRequest,
-  blindSign,
   deriveProof,
   initializeWasm,
-  sign,
-  unblind,
-  verify,
   verifyProof,
-  blindVerify,
 } from '../../lib';
 import { DeriveProofVcPair } from '../../src/js';
 
@@ -45,12 +39,6 @@ _:b1 <http://schema.org/name> "ABC inc." .
 <http://example.org/vcred/00> <https://www.w3.org/2018/credentials#issuanceDate> "2022-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
 <http://example.org/vcred/00> <https://www.w3.org/2018/credentials#expirationDate> "2025-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
 `;
-const proofWithoutProofvalue1 = `
-_:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/security#DataIntegrityProof> .
-_:b0 <http://purl.org/dc/terms/created> "2023-02-09T09:35:07Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
-_:b0 <https://w3id.org/security#proofPurpose> <https://w3id.org/security#assertionMethod> .
-_:b0 <https://w3id.org/security#verificationMethod> <did:example:issuer0#bls12_381-g2-pub001> .
-`;
 const proof1 = `
 _:b0 <https://w3id.org/security#proofValue> "ulyXJi_kpGXb2nUqVCRTzw03zFZyswkPLszC47yoRvUbGSkw2-v6GnY7X31hRYt4AnHL4DdyqBDvkUBbr0eTTUk3vNVI1LRxSfXRqqLng4Qx6SX7tptjtHzjJMkQnolGpiiFfE9k8OhOKcntcJwGSaQ"^^<https://w3id.org/security#multibase> .
 _:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/security#DataIntegrityProof> .
@@ -59,51 +47,7 @@ _:b0 <http://purl.org/dc/terms/created> "2023-02-09T09:35:07Z"^^<http://www.w3.o
 _:b0 <https://w3id.org/security#proofPurpose> <https://w3id.org/security#assertionMethod> .
 _:b0 <https://w3id.org/security#verificationMethod> <did:example:issuer0#bls12_381-g2-pub001> .
 `;
-
-describe('Proofs', () => {
-  test('sign', async () => {
-    await initializeWasm();
-
-    const signature = sign(doc1, proofWithoutProofvalue1, keyGraph);
-    console.log(`signature: ${signature}`);
-
-    expect(signature).toBeDefined();
-  });
-
-  test('verify', async () => {
-    await initializeWasm();
-
-    const verified = verify(doc1, proof1, keyGraph);
-    console.log(`verified: ${JSON.stringify(verified, null, 2)}`);
-
-    expect(verified.verified).toBeTruthy();
-  });
-
-  test('blind sign and verify', async () => {
-    await initializeWasm();
-
-    const secret = new Uint8Array(Buffer.from('SECRET'));
-    const nonce = 'NONCE';
-    const { request, blinding } = blindSignRequest(secret, nonce);
-    expect(request).toBeDefined();
-
-    const blindedProof = blindSign(
-      request,
-      nonce,
-      doc1,
-      proofWithoutProofvalue1,
-      keyGraph,
-    );
-    expect(blindedProof).toBeDefined();
-
-    const proof = unblind(doc1, blindedProof, blinding);
-    expect(proof).toBeDefined();
-
-    const verified = blindVerify(secret, doc1, proof, keyGraph);
-    expect(verified).toBeTruthy();
-  });
-
-  const doc2 = `
+const doc2 = `
 <http://example.org/vaccine/a> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://example.org/vocab/Vaccine> .
 <http://example.org/vaccine/a> <http://schema.org/name> "AwesomeVaccine" .
 <http://example.org/vaccine/a> <http://schema.org/manufacturer> <http://example.org/awesomeCompany> .
@@ -114,7 +58,7 @@ describe('Proofs', () => {
 <http://example.org/vicred/a> <https://www.w3.org/2018/credentials#issuanceDate> "2020-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
 <http://example.org/vicred/a> <https://www.w3.org/2018/credentials#expirationDate> "2023-12-31T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
 `;
-  const proof2 = `
+const proof2 = `
 _:b0 <https://w3id.org/security#proofValue> "uh-n1eUTNbs6fG9NMTPTL98zwcwfA1N4GCm0XXl__t5tMKOKU1LBfwt1f7Dtoy9dHnHL4DdyqBDvkUBbr0eTTUk3vNVI1LRxSfXRqqLng4Qx6SX7tptjtHzjJMkQnolGpiiFfE9k8OhOKcntcJwGSaQ"^^<https://w3id.org/security#multibase> .
 _:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/security#DataIntegrityProof> .
 _:b0 <https://w3id.org/security#cryptosuite> "bbs-termwise-signature-2023" .
@@ -122,7 +66,7 @@ _:b0 <http://purl.org/dc/terms/created> "2023-02-03T09:49:25Z"^^<http://www.w3.o
 _:b0 <https://w3id.org/security#proofPurpose> <https://w3id.org/security#assertionMethod> .
 _:b0 <https://w3id.org/security#verificationMethod> <did:example:issuer3#bls12_381-g2-pub001> .
 `;
-  const disclosedDoc1 = `
+const disclosedDoc1 = `
 _:e0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .
 _:e0 <http://example.org/vocab/isPatientOf> _:b0 .
 _:e0 <http://schema.org/worksFor> _:b1 .
@@ -135,14 +79,14 @@ _:e2 <https://www.w3.org/2018/credentials#issuer> <did:example:issuer0> .
 _:e2 <https://www.w3.org/2018/credentials#issuanceDate> "2022-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
 _:e2 <https://www.w3.org/2018/credentials#expirationDate> "2025-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
 `;
-  const disclosedProof1 = `
+const disclosedProof1 = `
 _:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/security#DataIntegrityProof> .
 _:b0 <https://w3id.org/security#cryptosuite> "bbs-termwise-signature-2023" .
 _:b0 <http://purl.org/dc/terms/created> "2023-02-09T09:35:07Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
 _:b0 <https://w3id.org/security#proofPurpose> <https://w3id.org/security#assertionMethod> .
 _:b0 <https://w3id.org/security#verificationMethod> <did:example:issuer0#bls12_381-g2-pub001> .
 `;
-  const disclosedDoc2 = `
+const disclosedDoc2 = `
 _:e1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://example.org/vocab/Vaccine> .
 _:e1 <http://schema.org/status> "active" .
 _:e3 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.w3.org/2018/credentials#VerifiableCredential> .
@@ -151,14 +95,14 @@ _:e3 <https://www.w3.org/2018/credentials#issuer> <did:example:issuer3> .
 _:e3 <https://www.w3.org/2018/credentials#issuanceDate> "2020-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
 _:e3 <https://www.w3.org/2018/credentials#expirationDate> "2023-12-31T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
 `;
-  const disclosedProof2 = `
+const disclosedProof2 = `
 _:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/security#DataIntegrityProof> .
 _:b0 <https://w3id.org/security#cryptosuite> "bbs-termwise-signature-2023" .
 _:b0 <http://purl.org/dc/terms/created> "2023-02-03T09:49:25Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
 _:b0 <https://w3id.org/security#proofPurpose> <https://w3id.org/security#assertionMethod> .
 _:b0 <https://w3id.org/security#verificationMethod> <did:example:issuer3#bls12_381-g2-pub001> .
 `;
-  const disclosedDoc1WithHiddenLiterals = `
+const disclosedDoc1WithHiddenLiterals = `
 _:e0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .
 _:e0 <http://example.org/vocab/isPatientOf> _:b0 .
 _:e0 <http://schema.org/worksFor> _:b1 .
@@ -173,6 +117,7 @@ _:e2 <https://www.w3.org/2018/credentials#issuanceDate> "2022-01-01T00:00:00Z"^^
 _:e2 <https://www.w3.org/2018/credentials#expirationDate> "2025-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
 `;
 
+describe('Proofs', () => {
   test('deriveProof', async () => {
     await initializeWasm();
 
@@ -330,59 +275,5 @@ _:c14n9 <https://w3id.org/security#proofValue> "uomFhWQnaAgAAAAAAAAAAiAvTWnfhK5F
     console.log(`verified: ${JSON.stringify(verified, null, 2)}`);
 
     expect(verified.verified).toBeTruthy();
-  });
-
-  const boundProof1 = `
-_:b0 <https://w3id.org/security#proofValue> "usYxFJJw9C0KHipWTTevDyU44iLEd6OWcqd1k33w0iuectnnNpDGS5D_kTULrexnpAWQCF5cBR1F0h3FXGsm2xh7Fafg49VG-Slte0XnTgDzpRqn0nqhO4I57s-b3TPVbA_t5uyJnGllyB6QcwVtRQA"^^<https://w3id.org/security#multibase> .
-_:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/security#DataIntegrityProof> .
-_:b0 <https://w3id.org/security#cryptosuite> "bbs-termwise-bound-signature-2023" .
-_:b0 <http://purl.org/dc/terms/created> "2023-02-09T09:35:07Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
-_:b0 <https://w3id.org/security#proofPurpose> <https://w3id.org/security#assertionMethod> .
-_:b0 <https://w3id.org/security#verificationMethod> <did:example:issuer0#bls12_381-g2-pub001> .
-`;
-  const disclosedBoundProof1 = `
-_:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/security#DataIntegrityProof> .
-_:b0 <https://w3id.org/security#cryptosuite> "bbs-termwise-bound-signature-2023" .
-_:b0 <http://purl.org/dc/terms/created> "2023-02-09T09:35:07Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
-_:b0 <https://w3id.org/security#proofPurpose> <https://w3id.org/security#assertionMethod> .
-_:b0 <https://w3id.org/security#verificationMethod> <did:example:issuer0#bls12_381-g2-pub001> .
-`;
-
-  test('deriveProof with secret', async () => {
-    await initializeWasm();
-
-    const deanonMap = new Map([
-      ['_:e0', '<did:example:john>'],
-      ['_:e1', '<http://example.org/vaccine/a>'],
-      ['_:e2', '<http://example.org/vcred/00>'],
-      ['_:e3', '<http://example.org/vicred/a>'],
-    ]);
-    const secret = new Uint8Array(Buffer.from('SECRET'));
-    const nonce = 'abcde';
-
-    const vcPair1: DeriveProofVcPair = {
-      originalDocument: doc1,
-      originalProof: boundProof1,
-      disclosedDocument: disclosedDoc1,
-      disclosedProof: disclosedBoundProof1,
-    };
-    const vcPair2: DeriveProofVcPair = {
-      originalDocument: doc2,
-      originalProof: proof2,
-      disclosedDocument: disclosedDoc2,
-      disclosedProof: disclosedProof2,
-    };
-
-    const req: DeriveProofRequest = {
-      secret,
-      vcPairs: [vcPair1, vcPair2],
-      deanonMap,
-      nonce,
-      keyGraph,
-    };
-    const vp = deriveProof(req);
-    console.log(`vp: ${vp}`);
-
-    expect(vp).toBeTruthy();
   });
 });
---
+++ tests/js/signatures.spec.ts
@@ -0,0 +1,69 @@
+import { initializeWasm, sign, verify } from '../../lib';
+
+const keyGraph = `
+# issuer0
+<did:example:issuer0> <https://w3id.org/security#verificationMethod> <did:example:issuer0#bls12_381-g2-pub001> .
+<did:example:issuer0#bls12_381-g2-pub001> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/security#Multikey> .
+<did:example:issuer0#bls12_381-g2-pub001> <https://w3id.org/security#controller> <did:example:issuer0> .
+<did:example:issuer0#bls12_381-g2-pub001> <https://w3id.org/security#secretKeyMultibase> "uekl-7abY7R84yTJEJ6JRqYohXxPZPDoTinJ7XCcBkmk" .
+<did:example:issuer0#bls12_381-g2-pub001> <https://w3id.org/security#publicKeyMultibase> "ukiiQxfsSfV0E2QyBlnHTK2MThnd7_-Fyf6u76BUd24uxoDF4UjnXtxUo8b82iuPZBOa8BXd1NpE20x3Rfde9udcd8P8nPVLr80Xh6WLgI9SYR6piNzbHhEVIfgd_Vo9P" .
+# issuer3
+<did:example:issuer3> <https://w3id.org/security#verificationMethod> <did:example:issuer3#bls12_381-g2-pub001> .
+<did:example:issuer3#bls12_381-g2-pub001> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/security#Multikey> .
+<did:example:issuer3#bls12_381-g2-pub001> <https://w3id.org/security#controller> <did:example:issuer3> .
+<did:example:issuer3#bls12_381-g2-pub001> <https://w3id.org/security#secretKeyMultibase> "uH1yGFG6C1pJd_N45wkOPrSNdvILdLm0c_0AXXRDGZy8" .
+<did:example:issuer3#bls12_381-g2-pub001> <https://w3id.org/security#publicKeyMultibase> "uidSE_Urr5MFE4SoqV3TZTBHPHM-tkpdRhBPrYeIbsudglVV_cddyEstHJOmSkfPOFsvEuA9qtWjFNpBebVSS4DPxBfNNWESSCz_vrnH62hbfpWdJSFR8YbqjborvpgM6" .    
+`;
+
+const doc1 = `
+<did:example:john> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .
+<did:example:john> <http://schema.org/name> "John Smith" .
+<did:example:john> <http://example.org/vocab/isPatientOf> _:b0 .
+<did:example:john> <http://schema.org/worksFor> _:b1 .
+_:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://example.org/vocab/Vaccination> .
+_:b0 <http://example.org/vocab/lotNumber> "0000001" .
+_:b0 <http://example.org/vocab/vaccinationDate> "2022-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
+_:b0 <http://example.org/vocab/vaccine> <http://example.org/vaccine/a> .
+_:b0 <http://example.org/vocab/vaccine> <http://example.org/vaccine/b> .
+_:b1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Organization> .
+_:b1 <http://schema.org/name> "ABC inc." .
+<http://example.org/vcred/00> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.w3.org/2018/credentials#VerifiableCredential> .
+<http://example.org/vcred/00> <https://www.w3.org/2018/credentials#credentialSubject> <did:example:john> .
+<http://example.org/vcred/00> <https://www.w3.org/2018/credentials#issuer> <did:example:issuer0> .
+<http://example.org/vcred/00> <https://www.w3.org/2018/credentials#issuanceDate> "2022-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
+<http://example.org/vcred/00> <https://www.w3.org/2018/credentials#expirationDate> "2025-01-01T00:00:00Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
+`;
+const proofWithoutProofvalue1 = `
+_:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/security#DataIntegrityProof> .
+_:b0 <http://purl.org/dc/terms/created> "2023-02-09T09:35:07Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
+_:b0 <https://w3id.org/security#proofPurpose> <https://w3id.org/security#assertionMethod> .
+_:b0 <https://w3id.org/security#verificationMethod> <did:example:issuer0#bls12_381-g2-pub001> .
+`;
+const proof1 = `
+_:b0 <https://w3id.org/security#proofValue> "ulyXJi_kpGXb2nUqVCRTzw03zFZyswkPLszC47yoRvUbGSkw2-v6GnY7X31hRYt4AnHL4DdyqBDvkUBbr0eTTUk3vNVI1LRxSfXRqqLng4Qx6SX7tptjtHzjJMkQnolGpiiFfE9k8OhOKcntcJwGSaQ"^^<https://w3id.org/security#multibase> .
+_:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/security#DataIntegrityProof> .
+_:b0 <https://w3id.org/security#cryptosuite> "bbs-termwise-signature-2023" .
+_:b0 <http://purl.org/dc/terms/created> "2023-02-09T09:35:07Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .
+_:b0 <https://w3id.org/security#proofPurpose> <https://w3id.org/security#assertionMethod> .
+_:b0 <https://w3id.org/security#verificationMethod> <did:example:issuer0#bls12_381-g2-pub001> .
+`;
+
+describe('Signatures', () => {
+  test('sign', async () => {
+    await initializeWasm();
+
+    const signature = sign(doc1, proofWithoutProofvalue1, keyGraph);
+    console.log(`signature: ${signature}`);
+
+    expect(signature).toBeDefined();
+  });
+
+  test('verify', async () => {
+    await initializeWasm();
+
+    const verified = verify(doc1, proof1, keyGraph);
+    console.log(`verified: ${JSON.stringify(verified, null, 2)}`);
+
+    expect(verified.verified).toBeTruthy();
+  });
+});
